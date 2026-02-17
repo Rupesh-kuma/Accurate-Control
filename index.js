e0b2cbaba348d326dd70bc06cef2435d4b6a98e1
Rupesh kumar <131702250+Rupesh-kuma@users.noreply.github.com>
2026-02-17 07:08:18 +0000

    update

--- index.js
+++ index.js
@@ -631,3 +631,24 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+//   sidebar menu=============================== 
+
+    const menuToggle = document.getElementById("menuToggle");
+    const sidePanel = document.getElementById("sidePanel");
+    const menuClose = document.getElementById("menuClose");
+    const menuOverlay = document.getElementById("menuOverlay");
+
+    menuToggle.addEventListener("click", () => {
+      sidePanel.classList.add("active");
+      menuOverlay.classList.add("active");
+    });
+
+    menuClose.addEventListener("click", () => {
+      sidePanel.classList.remove("active");
+      menuOverlay.classList.remove("active");
+    });
+
+    menuOverlay.addEventListener("click", () => {
+      sidePanel.classList.remove("active");
+      menuOverlay.classList.remove("active");
+    });
