e9561fe62e31f1f996065d31f212f25e70fcef39
Rupesh Kumar <rupesh.kumar.light@gmail.com>
2026-02-21 04:29:13 +0530

    update

--- index.js
+++ index.js
@@ -241,6 +241,26 @@ function loadPart(id, file) {
       sidePanel.classList.remove("active");
       menuOverlay.classList.remove("active");
     };
+
+
+    const openBtn = document.getElementById("openPopup");
+const closeBtn = document.getElementById("closePopup");
+const popup = document.getElementById("popup");
+
+openBtn.onclick = () => {
+  popup.style.display = "flex";
+}
+
+closeBtn.onclick = () => {
+  popup.style.display = "none";
+}
+
+// Background click se bhi close
+popup.onclick = (e) => {
+  if(e.target === popup){
+    popup.style.display = "none";
+  }
+}
   }
   
   loadPart("header", "./Header.html");
